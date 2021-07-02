67152fb48655ee4974460dc2cb6cb91ac60eac9d
enesertas <64091249+enesertas@users.noreply.github.com>
2021-07-03 00:08:18 +0300

    Update database.js

--- util/database.js
+++ util/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host:'localhost',
     user:'root',
-    password:'Qwaszxcv*1',
+    password:'',
     database:'mysql-nodejs-app'
     
 });
